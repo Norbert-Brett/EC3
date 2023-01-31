1422b1dd69181df0c7cd6c945168fcc67e328883
Norbert Brett <60483492+Norbert-Brett@users.noreply.github.com>
2023-01-31 18:18:39 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/vue.svg" class="logo" />
+    <HelloWorld msg="Welcome to Your Vue.js + EC3 App" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
